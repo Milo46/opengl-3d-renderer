b89da16ecdb6a78860b15398fcd5c2cadeab93f9
Milo46 <miloszbrzechczyn@gmail.com>
2023-01-24 16:16:23 +0100

    ImGui backend secured

--- application/src/ImGui/ImGuiBuild.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include <imgui.h>
-#include <backends/imgui_impl_opengl3.h>
-#include <backends/imgui_impl_glfw.h>
--- application/src/ImGui/ImGuiContext.cpp
+++ application/src/ImGui/ImGuiContext.cpp
@@ -1,6 +1,8 @@
 #include "ImGuiContext.hpp"
 
-#include "ImGuiBuild.hpp"
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+#include <backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
